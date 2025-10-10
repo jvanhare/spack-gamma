fd2a84d2fea8693186edcd860c3d770b2bde62ea
Julien Vanharen <julien.vanharen@inria.fr>
2025-10-10 08:57:58 +0200

    Add arcane framework

--- spack_repo/arcane/packages/arcane/package.py
+++ spack_repo/arcane/packages/arcane/package.py
@@ -1,9 +1,7 @@
 from spack.package import *
-from spack_repo.builtin.build_systems.cmake import (
-    CMakePackage,
-    CudaPackage,
-    ROCmPackage,
-)
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack_repo.builtin.build_systems.cuda import CudaPackage
+from spack_repo.builtin.build_systems.rocm import ROCmPackage
 
 
 class Arcane(CMakePackage, CudaPackage, ROCmPackage):
